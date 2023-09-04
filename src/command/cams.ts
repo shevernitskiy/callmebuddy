52f6345322aa9fab0f6641db1844e026c5c5b8de
shevernitskiy <mahahuha@ya.ru>
2023-09-04 07:24:55 +0300

    support new webcameras

--- src/command/cams.ts
+++ src/command/cams.ts
@@ -1,4 +1,4 @@
-import { CommandContext, Composer, Context, DOMParser, format, InputFile, InputMediaPhoto } from "../../deps.ts";
+import { CommandContext, Composer, Context, format, InputFile, InputMediaPhoto } from "../../deps.ts";
 import { BotContext } from "../bot.ts";
 
 import sources from "../data/cams.json" assert { type: "json" };
@@ -9,7 +9,7 @@ bot.command("cam", async (ctx) => {
   try {
     console.log(`Cams, id: ${ctx.msg!.from?.id}`);
     const tmp = await ctx.reply("фотографигуем...");
-    const media = await getInputMedias(sources);
+    const media = await getInputMedia(sources);
 
     if (media.length > 0) {
       await replyWithPost(ctx, media);
@@ -41,15 +41,22 @@ bot.command("everest", async (ctx) => {
   }
 });
 
-async function getInputMedias(sources: { code: string; name: string }[]): Promise<InputMediaPhoto<InputFile>[]> {
+async function getInputMedia(sources: { code: string; name: string }[]): Promise<InputMediaPhoto<InputFile>[]> {
   try {
-    const responses = await Promise.all(sources.map((item) => fetch(`https://rtsp.me/embed/${item.code}/`)));
-    const htmls = await Promise.all(responses.map((item) => item.text()));
+    const responses = await Promise.all(
+      sources.map((item) =>
+        fetch(
+          `https://api.codetabs.com/v1/proxy/?quest=https://e30.ru.cloud.trassir.com/thumbnail_get_tv?link=${item.code}`,
+        )
+      ),
+    );
+    const text = await Promise.all(responses.map((item) => item.text()));
+    const json = text.map((item) => JSON.parse(item));
     const out: InputMediaPhoto<InputFile>[] = [];
 
-    for (const [index, html] of htmls.entries()) {
-      const blob = await fetchToBlob(html);
-
+    for (const [index, item] of json.entries()) {
+      if (!item.thumbnails?.at(0)?.content) continue;
+      const blob = b64toBlob(item.thumbnails[0].content);
       if (blob === undefined) continue;
 
       out.push({
@@ -89,17 +96,24 @@ async function getInputMediasEverest(): Promise<InputMediaPhoto<InputFile>[]> {
   }
 }
 
-async function fetchToBlob(html: string): Promise<Blob | undefined> {
-  try {
-    const dom = new DOMParser().parseFromString(html, "text/html");
-    const source = dom?.querySelector("#video")?.getAttribute("poster");
-    if (source === undefined) return;
-    const res = await fetch(dom?.querySelector("#video")?.getAttribute("poster")!);
-    if (!res.ok) return undefined;
-    return await res.blob();
-  } catch (err) {
-    throw (err);
+function b64toBlob(data: string, content_type = "image/png", slice_size = 512): Blob {
+  const byte_characters = atob(data);
+  const byte_arrays = [];
+
+  for (let offset = 0; offset < byte_characters.length; offset += slice_size) {
+    const slice = byte_characters.slice(offset, offset + slice_size);
+
+    const byte_numbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byte_numbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byte_numbers);
+    byte_arrays.push(byteArray);
   }
+
+  const blob = new Blob(byte_arrays, { type: content_type });
+  return blob;
 }
 
 async function replyWithPost(ctx: CommandContext<Context>, out: InputMediaPhoto<InputFile>[]): Promise<void> {
@@ -110,7 +124,7 @@ async function replyWithPost(ctx: CommandContext<Context>, out: InputMediaPhoto<
       await ctx.reply("камеры оффлайн...");
     }
   } catch (err) {
-    throw (err);
+    throw err;
   }
 }
 
